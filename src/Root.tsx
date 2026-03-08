0032b288b8f2116dd2a4ccd94ee440583710705a
Egor Mamtsev <egormamcev@MacBook-Air.local>
2026-03-08 13:48:58 -0500

    done

---
+++ src/Root.tsx
@@ -0,0 +1,28 @@
+import { HashRouter as Router } from 'react-router-dom';
+import { App } from './App';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { TabsPage } from './components/TabsPage';
+import { Navigate } from 'react-router-dom';
+
+export const Root = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+
+        <Route path="home" element={<Navigate to="/" replace />} />
+
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  </Router>
+);
---
+++ src/components/TabsPage.tsx
@@ -0,0 +1,37 @@
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import tabs from '../store/tabs.json';
+
+export const TabsPage = () => {
+  const { tabId } = useParams();
+
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames({
+                'is-active': tab.id === tabId,
+              })}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="container">
+        <div className="block" data-cy="TabContent">
+          {selectedTab ? selectedTab.content : 'Please select a tab'}
+        </div>
+      </div>
+    </>
+  );
+};
